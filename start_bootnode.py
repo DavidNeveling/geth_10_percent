f019d58df6a4593e3cc9513d2cfdad0c5166922b
Neveling <david.neveling@vizientinc.com>
2022-05-22 19:51:28 -0500

    we

--- start_bootnode.py
+++ start_bootnode.py
@@ -6,13 +6,15 @@ import os, subprocess, gen_genesis, time, threading, signal, sys
 def launch_server():
     os.system('python3 -m http.server')
 
-def launch_node(datadir):
-    os.system('geth --datadir ' + datadir + ' --networkid 1231234512 --nat extip:`curl ipinfo.io/ip` --http.api web3,eth,debug,personal,net,admin,miner --http --http.addr "0.0.0.0" --http.port 8080 --http.corsdomain "*" --ipcdisable --port 30304 --verbosity 4 --http.vhosts "50-116-24-194.ip.linodeusercontent.com" --netrestrict "20.246.80.0/24","216.227.194.0/24","20.246.82.0/24","192.53.166.0/24","20.246.12.0/24" --mine --allow-insecure-unlock 2>/var/log/gethConsole.log 2>/var/log/gethConsole.log')
+def launch_node(datadir, vhost, restrict_list):
+    os.system('geth --datadir ' + datadir + ' --networkid 1231234512 --nat extip:`curl ipinfo.io/ip` --http.api web3,eth,debug,personal,net,admin,miner --http --http.addr "0.0.0.0" --http.port 8080 --http.corsdomain "*" --ipcdisable --port 30304 --verbosity 4 --http.vhosts "'+ vhost +'" --netrestrict ' + restrict_list + ' --mine --allow-insecure-unlock 2>/var/log/gethConsole.log 2>/var/log/gethConsole.log')
 
 
 # signal.signal(signal.SIGINT, signal_handler)
 
 datadir = './test/data'
+vhost = '192-53-166-228.ip.linodeusercontent.com'
+restrict_list = "20.246.80.0/24","216.227.194.0/24","20.246.82.0/24","192.53.166.0/24","20.246.12.0/24"
 if not os.path.exists(datadir):
     os.makedirs(datadir)
 password = 'a'
@@ -31,7 +33,7 @@ account_address = stdout[stdout.find(' ') + 1:]
 gen_genesis.create_genesis_file(account_address)
 os.system('geth init --datadir ' + datadir + ' genesis.json')
 x = threading.Thread(target=launch_server, args=())
-y = threading.Thread(target=launch_node, args=(datadir,))
+y = threading.Thread(target=launch_node, args=(datadir, vhost, restrict_list))
 x.start()
 y.start()
 y.join()
