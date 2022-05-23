36e24f01f051780a151186aebbcd933221ad200b
Neveling <david.neveling@vizientinc.com>
2022-05-22 20:01:10 -0500

    we

--- start_member_node.py
+++ start_member_node.py
@@ -1,8 +1,8 @@
 import os
 datadir = './test3/data'
-remotevhost = '192-53-166-228.ip.linodeusercontent.com'
+remotevhost = '192.53.166.228'
 selfvhost = '192-53-166-228.ip.linodeusercontent.com'
 os.system('wget -O genesis2.json ' + remotevhost + ':8000/genesis.json')
 os.system('./geth init --datadir '+datadir+' genesis2.json')
-os.system('./geth attach '+remotevhost+':8080 --exec admin.nodeInfo.enr > ENR') 
+os.system('./geth attach http://'+remotevhost+':8080 --exec admin.nodeInfo.enr > ENR') 
 os.system('''./geth --datadir '''+datadir+''' --networkid 1231234512 --bootnodes `cat ENR | tr -d '"'` --http --http.port 8545 --http.corsdomain "*" --http.vhosts "'''+selfvhost+'''" --http.api web3,eth,debug,personal,net,admin --vmdebug --http.addr "0.0.0.0" --allow-insecure-unlock''')
